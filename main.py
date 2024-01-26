108d01243db1d86fb3bf3fb9472a5d5e63e29b10
Fellype Pedersoli <fellypecosme@outlook.com>
2024-01-25 23:12:19 -0300

    Adding app_commands to the bot

--- main.py
+++ main.py
@@ -1,60 +1,70 @@
-import random, time, discord, re
+import random
+import time
+import discord
 
+from discord import app_commands
 from discord.ext import commands
-from discord import Message, app_commands
 
 from apikeys import *
 
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+bot = commands.Bot(command_prefix='!', intents = discord.Intents.all())
 
 @bot.event
 async def on_ready():
     print("Bot está rodando")
     print("________________")
 
-@bot.command()
-async def roll(ctx, dice: str, dice_modifier: int = 0):
+    try:
+        synced = await bot.tree.sync()
+        print(f'Synced {len(synced)} command(s)')
+    except Exception as e:
+        print(e)
 
-    type_and_times = re.findall('[0-9]+', dice)
+@bot.tree.command(name="hello")
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Olá, {interaction.user.mention}! Isso é um comando de barra")
 
-    times_to_roll = int(type_and_times[0])  if len(type_and_times) > 1 else 1
-    dice_type = int(type_and_times[1  if len(type_and_times) > 1 else 0])
-    
-    if times_to_roll == 1 or times_to_roll == "":
+@bot.tree.command(name="roll", description="Roll a dice")
+@app_commands.describe(dice_type = "Dice type", times_to_roll = "Times to roll the dice", dice_modifier = "Dice modifier")
+# async def roll(interaction: discord.Interaction, dice_type: int, times_to_roll: int = 1, dice_modifier: int = 0):
+#     final_dice = ""
+#     soma = 0
+#     for i in range(times_to_roll):
+#         dice_roll = random.randint(1, dice_type)
+#         dice_result = dice_roll + dice_modifier
+#         if dice_modifier > 0:
+#             final_dice += f'{dice_result} = [**{dice_roll}**+{dice_modifier}]\n'
+#             soma += dice_result
+#         else:
+#             final_dice += f'{dice_result} = [**{dice_roll}**{dice_modifier}]\n'
+#             soma += dice_result
+#     await interaction.response.send_message(f'__{times_to_roll}d{dice_type}__\n{final_dice}\nSoma: {soma}')
+async def roll(interaction: discord.Interaction, dice_type: int, times_to_roll: int = 1, dice_modifier: int = 0):
+    final_dice = ""
+    soma = 0
+    for i in range(times_to_roll):
         dice_roll = random.randint(1, dice_type)
         dice_result = dice_roll + dice_modifier
-        
-        if dice_modifier != 0:
-           if dice_modifier > 0:
-               await ctx.reply(f'**{dice_result}** = [{dice_roll}+{dice_modifier}]')
-           else:
-               await ctx.reply(f'**{dice_result}** = [{dice_roll}{dice_modifier}]')
+        if dice_modifier > 0:
+            final_dice += f'{dice_result} = [**{dice_roll}**+{dice_modifier}]\n'
+            final_dice = 0 if dice_result < 0 else final_dice
         else:
-           await ctx.reply(f'**{dice_result}** = [{dice_roll}]')
+            final_dice += f'{dice_result} = [**{dice_roll}**{dice_modifier}]\n'
+            final_dice = 0 if dice_result < 0 else final_dice
+        soma += dice_result
 
-    else:
-        final_dice = ""
-        soma = 0
-        for i in range(times_to_roll):
-            dice_roll = random.randint(1, dice_type)
-            dice_result = dice_roll + dice_modifier
-            if dice_modifier != 0:
-                if dice_modifier > 0:
-                    final_dice += f'{dice_result} = [**{dice_roll}**+{dice_modifier}]\n'
-                    soma += dice_result
-                else:
-                    final_dice += f'{dice_result} = [**{dice_roll}**{dice_modifier}]\n'
-                    soma += dice_result
-            else:
-                final_dice += f'{dice_result} = [**{dice_roll}**]\n'
-                soma += dice_result
-        await ctx.reply(f'{final_dice}\nSoma: {soma}')
+    modifier_str = f' {"" if dice_modifier == 0 else "+" if dice_modifier > 0 else ""}{dice_modifier}' if dice_modifier != 0 else ""
+    await interaction.response.send_message(f'__**{times_to_roll}d{dice_type}{modifier_str}**__\n{final_dice}\nSoma: {soma}')
+
 
 @bot.command()
 async def ajuda(ctx):
-    await ctx.author.send("Os comandos disponíveis são:\n\n !roll")
+    await ctx.author.send("Os comandos disponíveis são:\n !ajuda\n !roll\n !ping")
     time.sleep(1)
     await ctx.message.delete()
-    
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong")
 
 bot.run(TOKEN)
